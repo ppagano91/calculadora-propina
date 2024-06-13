76d28bca45054b59d0e6b89befe2522d11b4a1e2
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-06-13 14:56:05 -0300

    Listando Items

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { menuItems } from "./data/db"
+import MenuItem from "./components/MenuItem"
+
 function App() {
 
   return (
     <>
-      <h1>Hola Mundo!</h1>
+    <header className="bg-teal-400 py-5">
+      <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+    </header>
+    <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+      <div className="p-5">
+        <h2 className="text-4xl font-bold">Menú</h2>
+        <div className="space-y-3 mt-10">
+          {menuItems.map(item =>(
+            <MenuItem
+            key={item.id}
+            item={item}
+            />
+            ))}
+        </div>
+      </div>
+
+      <div>
+        <h2>Consumo</h2>
+      </div>
+
+    </main>
     </>
   )
 }
